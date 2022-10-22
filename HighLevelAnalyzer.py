b11a555976db66424de10f21d8412aeeeb60b93e
MaZaY-0 <116427642+MaZaY-0@users.noreply.github.com>
2022-10-22 15:44:08 +0700

    Update HighLevelAnalyzer.py

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -3,41 +3,70 @@
 
 from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
 
-
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class Hla(HighLevelAnalyzer):
+
     # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
     result_types = {
-        'i2c-address': {
-            'format': '{{data.description}}'
-        },
-        'i2c-data': {
-            'format': '{{data.description}}'
+            'error': {
+                'format': 'Error!'
+            },
+            "hi2c": {
+                'format': 'address: {{data.address}}; data_[{{data.count}}]: [ {{data.data}} ]'
+            }
         }
-    }
+        
+    
+    temp_frame = None
 
     def __init__(self):
+        '''
+        Initialize HLA.
+        Settings can be accessed using the same name used above.
+        '''
         pass
 
     def decode(self, frame: AnalyzerFrame):
-        if frame.type == 'address':
-            direction = 'Read' if frame.data['read'] else 'Write'
-            address = (frame.data['address'][0] << 1) | (1 if frame.data['read'] else 0)
-            message = ''
-            if 'ack' in frame.data:
-                message = 'ACK' if frame.data['ack'] else 'NAK'
-            elif 'error' in frame.data:
-                message = frame.data['error']
-            return AnalyzerFrame('i2c-address', frame.start_time, frame.end_time, {
-                'description': 'Setup {} to [0x{:02X}] + {}'.format(direction, address, message)
-            })
-        elif frame.type == 'data':
-            data = frame.data['data'][0]
-            message = ''
-            if 'ack' in frame.data:
-                message = 'ACK' if frame.data['ack'] else 'NAK'
-            elif 'error' in frame.data:
-                message = frame.data['error']
-            return AnalyzerFrame('i2c-data', frame.start_time, frame.end_time, {
-                'description': '0x{:02X} + {}'.format(data, message)
-            })
+        '''
+        Process a frame from the input analyzer, and optionally return a single `AnalyzerFrame` or a list of `AnalyzerFrame`s.
+        The type and data values in `frame` will depend on the input analyzer.
+        '''
+        # set our frame to an error frame, which will eventually get over-written as we get data.
+        if self.temp_frame is None:
+            self.temp_frame = AnalyzerFrame("error", frame.start_time, frame.end_time, {
+                "address": "error",
+                "data": "",
+                "count": 0
+            }
+            )
+
+        if frame.type == "start" or (frame.type == "address" and self.temp_frame.type == "error"):
+            frame_to_flush = None
+            if frame.type == "start" and self.temp_frame.type != "error":
+                # the previous frame hasn't been flushed yet. Likely a repeated start event.
+                frame_to_flush = self.temp_frame
+            self.temp_frame = AnalyzerFrame("hi2c", frame.start_time, frame.end_time, {
+                    "data": "",
+                    "count": 0
+                }
+            )
+            return frame_to_flush
+
+        if frame.type == "address":
+            self.temp_frame.end_time = frame.end_time
+            address_byte = frame.data["address"][0]
+            self.temp_frame.data["address"] = hex(address_byte)
+
+        if frame.type == "data":
+            self.temp_frame.end_time = frame.end_time
+            data_byte = frame.data["data"][0]
+            self.temp_frame.data["count"] += 1
+            if len(self.temp_frame.data["data"]) > 0:
+                self.temp_frame.data["data"] += ", "
+            self.temp_frame.data["data"] += hex(data_byte)
+
+        if frame.type == "stop":
+            self.temp_frame.end_time = frame.end_time
+            new_frame = self.temp_frame
+            self.temp_frame = None
+            return new_frame
